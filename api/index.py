5d2dbbd924cd3685ebc1110074c1142a5e7bd056
JeremyWebb45 <jeremy.webb429@gmail.com>
2023-11-13 16:53:30 -0700

    idk bruh

--- api/index.py
+++ api/index.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/api/python")
+@app.route("/api")
 def hello_world():
     return "<p>Hello, World!</p>"
